079f1cf44f68ab1450cae99be9f4bccd18ad80f3
TsanitaSafiera <wiekecahyadi@gmail.com>
2023-05-26 16:56:32 -0400

    Class04

--- src/test/java/Utils/CommonMethods.java
+++ src/test/java/Utils/CommonMethods.java
@@ -2,19 +2,31 @@ package Utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CommonMethods {
     public static WebDriver driver;
-    public static void openBrowserAndLaunchApplication(String url){
+    public static void openBrowserAndLaunchApplication(String URL, String browser){
 
-        // create instance
-        driver= new ChromeDriver();
-        // navigate to url
-        driver.get(url);
-        //maximize the screen
+        switch (browser){
+            case "chrome":
+                driver=new ChromeDriver();
+                break;
+
+            case "firefox":
+                driver=new FirefoxDriver();
+                break;
+        }
+//       maximize thhe window
         driver.manage().window().maximize();
-    }
-    public static void closeBrowser(){
-        driver.quit();
-    }
+        //navigate to the url
+        driver.get(URL);
+        }
+
+       public  static void closeBrowser(){
+        if(driver!=null){
+            driver.quit();
+        }
+       }
+
 }
